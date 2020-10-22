4808aa2a185c315fae698b9b52d56c92b22d28f7
DESKTOP-4B4UGNN\USER <pagrau9@misena.edu.co>
2020-10-21 22:56:07 -0500

    Mi primer archivo desde Sourtreen

---
+++ codigo.js
@@ -0,0 +1,13 @@
+// Una ventana energente de alerta 
+//alert("Hola Mundo");
+
+// Ventana energente posterior ala inicial
+
+//alert("Segunda Ventana Energente");
+
+var num1=0;
+var num2=5;
+var resultado= num1+num2;
+
+
+
